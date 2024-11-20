d2335a52baafbafc3fcc128f33325441fe7cf720
Raphael-Musau <rwmusaulito209@gmail.com>
2024-11-20 20:42:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Contact Form functionality
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out!');
+});
